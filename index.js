40972d508e090cd6e98736bb1afedbd0c709381c
alex kimathi <keemalekie@gmail.com>
2022-06-02 21:27:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input')
+    input.addEventListener('click',clickAlert)
+}
+addingEventListener();
+
+const clickAlert = () => {
+    alert('I was clicked!')
 }
